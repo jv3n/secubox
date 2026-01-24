c2d7569061ac753c11e3a0278df3b79894a631f4
Julien Venet <venet.julien@gmail.com>
2026-01-24 09:12:46 -0500

    feat(front): add collapsible sidebar with navigation menu

--- frontend/secubox-web/src/app/view/home-view.component.ts
+++ frontend/secubox-web/src/app/view/home-view.component.ts
@@ -1,14 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { FileSystemComponent } from '../features/file-system/file-system.component';
 
 @Component({
   selector: 'sb-home',
-  template: `
-    <main class="home">
-      <sb-file-system />
-    </main>
-  `,
-  imports: [FileSystemComponent],
+  templateUrl: './home-view.component.html',
+  styleUrl: './home-view.component.scss',
+  imports: [FileSystemComponent, MatIconModule, MatButtonModule, MatTooltipModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class HomeComponent {}
+export default class HomeComponent {
+  isMenuCollapsed = signal<boolean>(false);
+
+  toggleMenu(): void {
+    this.isMenuCollapsed.update((collapsed) => !collapsed);
+  }
+}
